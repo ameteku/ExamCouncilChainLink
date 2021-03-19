e7d27f42a6a342e50b2233ea87548260e201883e
michael ameteku <Ameteku@github.com>
2021-03-19 16:17:53 -0400

    add simple definition for candidate

--- smartContracts/exam_items/Exam.sol
+++ smartContracts/exam_items/Exam.sol
@@ -1,1 +1,10 @@
-Exam.sol
+pragma solidity ^0.5.0;
+
+/**
+ * The Exam contract does this and that...
+ */
+contract Exam {
+  constructor() public {
+    
+  }
+}
--- smartContracts/student_items/Candidate.sol
+++ smartContracts/student_items/Candidate.sol
@@ -1,1 +1,12 @@
-Candidate.sol
+pragma solidity ^0.5.0;
+
+/**
+ * The Candidate
+ contract does this and that...
+ */
+contract Candidate {
+  constructor() public {
+    
+  }
+}
+
