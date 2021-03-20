a7760a819a44afd6e86a4b735779939da58d822a
michael ameteku <Ameteku@github.com>
2021-03-19 23:59:43 -0400

    Add code for exam council

--- smartContracts/exam_items/exam_governance.sol
+++ smartContracts/exam_items/exam_governance.sol
@@ -1,10 +1,69 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.7.0 <0.8.0;
 
-/**
- * The ExamGovernment contract does this and that...
- */
-contract ExamGovernment {
-  constructor() public {
+struct  Exam {
+    string  id;
+}
+
+contract ExamCouncil {
     
-  }
+    string[] candidates;
+    string[] examIds;
+    Exam[] exams;
+    
+    constructor (string memory candidateIds, string memory firstExamId) public {
+        require(bytes(candidateIds).length != 0);
+        require( bytes(firstExamId).length != 0);
+        
+        //for( int i = 0; i<5 ; i++) {
+            candidates.push(candidateIds);
+        //}
+        
+        exams.push( Exam(firstExamId));
+        examIds.push(firstExamId);
+    }
+    
+    function writeExam(string memory candidateId, string memory fingerprint, string memory examId) 
+    public view returns(string memory, bool) {
+        require(bytes(candidateId).length != 0 );
+        require(bytes(fingerprint).length != 0 );
+        require(bytes(examId).length != 0 );
+        
+        string memory candidateWriting;
+        string memory currentExamId;
+        
+        for(uint i = 0; i< candidates.length; i++) {
+            if(keccak256(abi.encodePacked(candidateId)) == keccak256(abi.encodePacked(candidates[i]))) {
+                candidateWriting = candidateId;
+                break;
+            }
+        }
+        
+        // return if candidate not found
+        if(bytes(candidateWriting).length == 0 ) {
+            return ("Candidate Not Found", false);
+        }
+        
+        //finding exam
+        for(uint i = 0; i< examIds.length; i++) {
+            if(keccak256(abi.encodePacked(examId)) == keccak256(abi.encodePacked(examIds[i]))) {
+                currentExamId = examId;
+                break;
+            }
+        }
+        
+        // return if exam not found
+        if(bytes(currentExamId).length == 0 ) {
+            return ("Exam not found", false);
+        }
+        
+        int score;
+        //Exam currentExam;
+        for (uint i = 0; i< exams.length; i++) {
+            if(keccak256(abi.encodePacked(currentExamId)) == keccak256(abi.encodePacked(examIds[i]))) {
+            //score = exams[i].write();
+            
+            return ('Exam written', true);
+        }
+    }
+}
 }
--- smartContracts/student_items/Candidate.sol
+++ smartContracts/student_items/Candidate.sol
@@ -1,12 +1,47 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.7.0 <0.8.0;
+pragma experimental ABIEncoderV2;
 
 /**
  * The Candidate
  contract does this and that...
  */
-contract Candidate {
-  constructor() public {
     
+contract Candidate {
+     struct Exam {
+        string examId;
+        uint score;
+        bool written;
+    }
+    
+    uint id;
+    string studentSignature;
+    Exam[] exams;
+    
+  constructor(string memory signature,string memory examId) public {
+      require(bytes(signature).length != 0);
+      id = 1;
+      
+      studentSignature = signature;
+      
+      if(bytes(examId).length != 0) {
+         exams.push( Exam(examId,0, false ));
+      }
+  }
+  
+  function getParticularExam(uint candidateId, string memory examId ) public view returns(Exam memory) {
+      require(keccak256(abi.encodePacked(candidateId)) == keccak256(abi.encodePacked(id)));
+      require(bytes(examId).length != 0);
+      
+      for(uint i = 0; i< exams.length; i++)
+      {
+          if(keccak256(abi.encodePacked(exams[i].examId)) ==keccak256(abi.encodePacked(examId)))
+          {
+              return exams[i];
+          }
+      }
+      Exam memory empty;
+      
+      return empty;
   }
 }
 
