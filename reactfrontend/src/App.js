4093943d49d1de085aa252146cd87f67dbca54ab
Gianna Torpey <Gianna.Torpey@gmail.com>
2021-04-04 13:47:13 -0400

    Added login js component and route

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import {Route, Link} from "react-router-dom";
 import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
     </div>
   );
 }
--- reactfrontend/src/Home.js
+++ reactfrontend/src/Home.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 function Home(){
     return(
         <div className="Home">
-        <h1>Welcome to the Homepage</h1>    
+            <h1>Welcome to the Homepage</h1>    
         </div>
     )
 }
