7b56ee32156e273bd742247ff15196713361a0fa
Gianna Torpey <Gianna.Torpey@gmail.com>
2021-04-04 23:59:41 -0400

    Testing Commit 2

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -10,6 +10,7 @@ import Scores from "./Scores";
 import ExamRegister from "./ExamRegister";
 
 function App() {
+  
   return (
     <div className="App">
       <Route exact path="/" component={Home}/>
