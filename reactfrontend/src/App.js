c07f70e7db19a1b6e556eda0068d5c6bb637e3bd
Gianna Torpey <Gianna.Torpey@gmail.com>
2021-04-04 20:47:43 -0400

    Added registration and student landing page route

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 import {Route, Link} from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
+import Register from "./Register";
+import Student from "./Student";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Home}/>
       <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+      <Route exact path="/student/home" component={Student}/>
     </div>
   );
 }
--- reactfrontend/src/Register.js
+++ reactfrontend/src/Register.js
@@ -1,28 +1,26 @@
 import React from "react";
-import ReactDom from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css';
+import './Register.css';
 
-function Login(){
-    function loginUser(event){
+function Register(){
+    function registerUser(event){
 
     }
     return (
         <div className="login text-center">
             <form class="form-signin">
-    <h1>{userType}</h1>
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Register Your Details</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus/>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
       <div class="checkbox mb-3">
       </div>
-      <button class="btn btn-lg btn-primary btn-block" onClick={loginUser} type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" onClick={registerUser} type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
     </form>
         </div>
     )
 }
 
-export default Login;
+export default Register;
