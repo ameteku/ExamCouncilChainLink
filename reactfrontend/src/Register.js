f210630b9e9bb1626761c0301a9bea5efab1eb45
Gianna Torpey <Gianna.Torpey@gmail.com>
2021-04-10 10:13:18 -0400

    Linked Login and Register pages

--- reactfrontend/src/Register.js
+++ reactfrontend/src/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Register.css';
+import {Link} from "react-router-dom";
 
 function Register(){
     function registerUser(event){
@@ -9,14 +10,22 @@ function Register(){
     return (
         <div className="login text-center">
             <form class="form-signin">
-      <h1 class="h3 mb-3 font-weight-normal">Register Your Details</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus/>
+      <h1 class="h1 mb-3 font-weight-normal">Register Your Details</h1>
+      <label for="inputStudentID" class="sr-only">Student ID</label>
+      <input type="text" id="inputStudentID" class="form-control" placeholder="Student ID" required autofocus/>
+      
+      <label for="inputFirstName" class="sr-only">First Name</label>
+      <input type="text" id="inputFirstName" class="form-control" placeholder="First Name" required autofocus/>
+      
+      <label for="inputLastName" class="sr-only">Last Name</label>
+      <input type="text" id="inputLastName" class="form-control" placeholder="Last Name" required autofocus/>
+      
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
       <div class="checkbox mb-3">
       </div>
-      <button class="btn btn-lg btn-primary btn-block" onClick={registerUser} type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" onClick={registerUser} type="submit">Register</button>
+      <Link to="/"><button class="reg-button btn btn-lg btn-primary btn-block " type="submit">Back to Login</button></Link>
       <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
     </form>
         </div>
